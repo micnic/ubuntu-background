6ce08f557e4ca75c1fb5f21df45f46a380198ec0
micnic <nicu.micleusanu@sendmoments.de>
2014-10-28 00:44:24 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+Generator for Ubuntu background xml
+
+run `node generator.js`
+
+will get backgrounds from `/usr/share/backgrounds` and create `/usr/share/backgrounds/contest/generated.xml`
+
+background duration: 55 seconds
+transition duration: 3 seconds
---
+++ generator.js
@@ -0,0 +1,45 @@
+var fs = require('fs'),
+	path = require('path');
+
+fs.readdir('/usr/share/backgrounds', function (error, files) {
+
+	var content = '';
+
+	content += '<background>\n';
+	content += '\t<starttime>\n';
+	content += '\t\t<year>1970</year>\n';
+	content += '\t\t<month>01</month>\n';
+	content += '\t\t<day>01</day>\n';
+	content += '\t\t<hour>00</hour>\n';
+	content += '\t\t<minute>00</minute>\n';
+	content += '\t\t<second>00</second>\n';
+	content += '\t</starttime>\n';
+
+	files.filter(function (element) {
+		return /\.jpg$/.test(element);
+	}).forEach(function (element, index, array) {
+
+		var current = path.join('/', __dirname, element);
+
+		content += '\t<static>\n';
+		content += '\t\t<duration>55.0</duration>\n';
+		content += '\t\t<file>' + current + '</file>\n';
+		content += '\t</static>\n';
+
+		content += '\t<transition>\n';
+		content += '\t\t<duration>3.0</duration>\n';
+		content += '\t\t<from>' + current + '</from>\n';
+
+		if (index < array.length - 1) {
+			content += '\t\t<to>' + path.join('/', __dirname, array[index + 1]) + '</to>\n';
+		} else {
+			content += '\t\t<to>' + path.join('/', __dirname, array[0]) + '</to>\n';
+		}
+
+		content += '\t</transition>\n';
+	});
+
+	content += '</background>';
+
+	fs.writeFile('/usr/share/backgrounds/contest/generated.xml', content);
+});
